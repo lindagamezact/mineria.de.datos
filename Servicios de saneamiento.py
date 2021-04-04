43ba9f06a5722659502af089db1a6f43598a19a2
lindagamez <77224859+lindagamez@users.noreply.github.com>
2021-04-04 18:40:24 -0500

    Avance

--- Servicios de saneamiento.py
+++ Servicios de saneamiento.py
@@ -7,6 +7,12 @@ from tabulate import tabulate
 import statistics
 import numpy as np
 
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+from scipy.stats import shapiro
+from scipy.stats import bartlett
+
 def print_tabulate(df: pd.DataFrame):
     print(tabulate(df, headers=df.columns, tablefmt='orgtbl'))
 
@@ -16,8 +22,11 @@ df = df.drop(['Display Value'], axis=1)
 df = df.drop(['PUBLISH STATES'], axis=1)
 #print(df.columns)
 df.columns =['Indicador','Anio', 'Region de la OMS', 'Pais', 'Tipo del Area', 'Porcentaje']
-num=['Anio','Porcentaje']
-df[num] = df[num].apply(pd.to_numeric, errors='coerce', axis=1)
+pd.to_numeric(df['Porcentaje'])
+pd.to_numeric(df['Anio'],downcast='integer')
+
+#num=['Anio','Porcentaje']
+#df[num] = df[num].apply(pd.to_numeric, errors='coerce', axis=1)
 
 df['Categoria'] = np.where(df['Indicador']=="Population using safely managed sanitation services (%)", 'Servicios de saneamiento gestionados de forma segura ', 'Al menos servicios basicos de saneamiento ')
 
@@ -25,15 +34,269 @@ df.to_csv("C:/Users/linda/OneDrive/Documentos/LINDA GAMEZ/7MO SEMESTRE/MINERÍA 
 
 df_complete = pd.read_csv("C:/Users/linda/OneDrive/Documentos/LINDA GAMEZ/7MO SEMESTRE/MINERÍA DE DATOS/mineria.de.datos/cvs/Basic and safely managed sanitation services limpio.csv")
 
-#Promedio por region y tipo de area y Categoria
-df_by_dep = df_complete.groupby(["Region de la OMS","Categoria","Tipo del Area"])[["Porcentaje"]].aggregate(statistics.mean)
+#Funcion para el formato del eje x (Años) de las gráficas
+def gxanio_format(df: pd.DataFrame): 
+    plt.xticks(df.index)
+    plt.xticks(rotation ='vertical')
+    plt.xlabel("Años")
 
-#Minimo por region y tipo de area
-#df_by_dep = df_complete.groupby(["Region de la OMS","Categoria","Tipo del Area"])[["Porcentaje"]].aggregate(min)
+#Funcion para las graficas cuando la agrupacion se conforma de los años y 1 categoría más
+def plot_by_A(df: pd.DataFrame, cat:str,lar:str,ag:str,a:str)->None: 
+    df[df[ag] == cat].plot(y =lar) 
+    gxanio_format(df)
+    plt.title(cat)    
+    plt.savefig(f"C:/Users/linda/OneDrive/Documentos/LINDA GAMEZ/7MO SEMESTRE/MINERÍA DE DATOS/mineria.de.datos/img/{a}_{lar}_{cat}.png",bbox_inches='tight')
+    plt.close()
+    df[df[ag] == cat].boxplot(lar,by =ag)
+    plt.savefig(f"C:/Users/linda/OneDrive/Documentos/LINDA GAMEZ/7MO SEMESTRE/MINERÍA DE DATOS/mineria.de.datos/img/{a}_{lar}_bplt_{cat}.png",bbox_inches='tight')
+    plt.close()
 
-#Maximo por region y tipo de area
-#df_by_dep = df_complete.groupby(["Region de la OMS","Categoria","Tipo del Area"])[["Porcentaje"]].aggregate(max)
+#Función para el ANOVA cuando la agrupacion se conforma de los años y 1 categoría más
+def anova(a:str,b:str,df:pd.DataFrame): 
+    modl = ols(f"{a} ~ {b}", data=df).fit()
+    anova_df = sm.stats.anova_lm(modl, typ=2)
+    if anova_df["PR(>F)"][0] < 0.05:
+        print(f"Estadistico: {a}")
+        print("El ANOVA nos dice que SI hay diferencias")
+        #print(anova_df)
+        # Prueba tukey
+    else:
+        print(f"Estadistico: {a}")       
+        print("El ANOVA nos dice que NO hay diferencias")
+        #print(anova_df)
+    
+    #Prueba Normalidad Shapiro-Wilk test
+    statSW,pSW = shapiro(modl.resid)
+    print("Para el supuesto de normalidad se hizo el Shapiro-Wilk test y obtuvimos: ")
+    if pSW < 0.05: 
+        print(f"Los residuales del estadístico <{a}> NO son normales")
+        #Levene´s test
+    else: 
+        print(f"Los residuales del estadístico <{a}> SI son normales")
+        #Bartlett's test
+        #statB, pB = bartlett()
 
-df_by_dep.reset_index(inplace=True)
-#df_by_dep.set_index("Categoria", inplace=True)
-print_tabulate(df_by_dep)
+    #Prueba de Homogeneidad de Varianzas
+    #Bartlett’s test (para normales)
+    #Levene´s test (no normales)
+
+    #Prueba de independencia
+
+#-------------------------------------------------------------AC-----------------------------------------------------------------------------
+#AC es agrupación por año y categoría tomando en cuenta como dato numérico el porcentaje de la población que utiliza servicios de 
+# saneamiento gestionados de forma segura para el calculo de los estadísticos 
+#(Media/Mediana/Variación/Dispersión/Mínimo/Máximo) del porcentaje de la población de cierta categoría (usan al menos.../usan servicios de saneamiento...) en cierto año 
+
+def TE_AC(*op1): 
+    lis_op=[] #Lista para juntar todos los estadisticos en un DataFrame
+    for op in op1:
+        df_by_AC = df_complete.groupby(["Anio","Categoria"])[["Porcentaje"]].aggregate(op)
+        df_by_AC.reset_index(inplace=True)
+        lis_op.append(df_by_AC['Porcentaje']) #Agrega datos a la lista
+    #DataFrame con todos los estadisticos
+    com = pd.DataFrame({"Categoria":df_by_AC['Categoria'],
+        "Promedio":lis_op[0],"Mediana":lis_op[1],"Varianza":lis_op[2],"Desviacion_Estandar":lis_op[3],"Minimo":lis_op[4],"Maximo":lis_op[5] })
+    com.set_index(df_by_AC['Anio'], inplace=True)
+    print_tabulate(com.head(8))
+
+    agrupar = "Categoria"
+    aA = "AC"
+
+    #Un estadístico por boxplot
+    for lis in com.columns[1:len(com.columns)]:
+        com.boxplot(lis,by=agrupar)
+        plt.xticks(rotation =5)
+        plt.tight_layout()
+        plt.savefig(f"C:/Users/linda/OneDrive/Documentos/LINDA GAMEZ/7MO SEMESTRE/MINERÍA DE DATOS/mineria.de.datos/img/{aA}_{lis}_boxplot.png")
+        plt.close()
+    
+    for cat in set(com[agrupar]):
+        for lar in com.columns[1:len(com.columns)]: 
+            plot_by_A(com, cat,lar,agrupar,aA)
+    
+    #ANOVA para cada uno de los estadisticos considerados  
+    for an in com.columns[1:len(com.columns)]:
+        anova(an,agrupar,com)  
+    
+TE_AC(statistics.mean,statistics.median,statistics.variance,statistics.stdev,min,max)
+
+#-------------------------------------------------------------AR-----------------------------------------------------------------------------
+#AR es agrupación por año y región de la OMS tomando en cuenta como dato numérico el porcentaje de la población que utiliza servicios de 
+#saneamiento gestionados de forma segura para el calculo de los estadísticos 
+#(Media/Mediana/Variación/Dispersión/Mínimo/Máximo) del porcentaje de la población de cierta región (usan al menos.../usan servicios de saneamiento...) en cierto año 
+
+def TE_AR(*op1): #Año, region
+    lis_op=[] 
+    for op in op1:
+        df_by_AR = df_complete.groupby(["Anio","Region de la OMS"])[["Porcentaje"]].aggregate(op)
+        df_by_AR.reset_index(inplace=True)
+        lis_op.append(df_by_AR['Porcentaje']) 
+    #DataFrame con todos los estadisticos
+    com = pd.DataFrame({"Region":df_by_AR['Region de la OMS'],
+        "Promedio":lis_op[0],"Mediana":lis_op[1],"Varianza":lis_op[2],"Desviacion_Estandar":lis_op[3],"Minimo":lis_op[4],"Maximo":lis_op[5]})
+    com.set_index(df_by_AR['Anio'], inplace=True)
+    print_tabulate(com.head(10))
+
+    agrupar = "Region"
+    aA = "AR"
+
+    #Un estadístico por boxplot
+    for lis in com.columns[1:len(com.columns)]:
+        com.boxplot(lis,by=agrupar)
+        plt.xticks(rotation =18)
+        plt.tight_layout()
+        plt.savefig(f"C:/Users/linda/OneDrive/Documentos/LINDA GAMEZ/7MO SEMESTRE/MINERÍA DE DATOS/mineria.de.datos/img/{aA}_{lis}_boxplot.png")
+        plt.close()
+    
+    #Graficas
+    for cat in set(com[agrupar]):
+        for lar in com.columns[1:len(com.columns)]: 
+            plot_by_A(com, cat,lar,agrupar,aA)
+    
+    #ANOVA para cada uno de los estadisticos considerados  
+    for an in com.columns[1:len(com.columns)]:
+        anova(an,agrupar,com)  
+    
+TE_AR(statistics.mean,statistics.median,statistics.variance,statistics.stdev,min,max)
+
+#-------------------------------------------------------------AT-----------------------------------------------------------------------------
+#AT es agrupación por año y tipo de area tomando en cuenta como dato numérico el porcentaje de la población que utiliza servicios de 
+#saneamiento gestionados de forma segura para el calculo de los estadísticos 
+#(Media/Mediana/Variación/Dispersión/Mínimo/Máximo) del porcentaje de la población de cierta región (usan al menos.../usan servicios de saneamiento...) en cierto año 
+
+def TE_AT(*op1): 
+    lis_op=[] 
+    for op in op1:
+        df_by_AT = df_complete.groupby(["Anio","Tipo del Area"])[["Porcentaje"]].aggregate(op)
+        df_by_AT.reset_index(inplace=True)
+        lis_op.append(df_by_AT['Porcentaje']) 
+    #DataFrame con todos los estadisticos
+    com = pd.DataFrame({"Area":df_by_AT['Tipo del Area'],
+        "Promedio":lis_op[0],"Mediana":lis_op[1],"Varianza":lis_op[2],"Desviacion_Estandar":lis_op[3],"Minimo":lis_op[4],"Maximo":lis_op[5]})
+    com.set_index(df_by_AT['Anio'], inplace=True)
+    print_tabulate(com.head(10))
+
+    agrupar = "Area"
+    aA = "AT"
+
+    #Un estadístico por boxplot
+    for lis in com.columns[1:len(com.columns)]:
+        com.boxplot(lis,by=agrupar)
+        plt.xticks(rotation =18)
+        plt.tight_layout()
+        plt.savefig(f"C:/Users/linda/OneDrive/Documentos/LINDA GAMEZ/7MO SEMESTRE/MINERÍA DE DATOS/mineria.de.datos/img/{aA}_{lis}_boxplot.png")
+        plt.close()
+    
+    #Graficas
+    for cat in set(com[agrupar]):
+        for lar in com.columns[1:len(com.columns)]: 
+            plot_by_A(com, cat,lar,agrupar,aA)
+
+    #ANOVA para cada uno de los estadisticos considerados
+    for an in com.columns[1:len(com.columns)]:
+        anova(an,agrupar,com)  
+
+TE_AT(statistics.mean,statistics.median,statistics.variance,statistics.stdev,min,max)
+
+#-------------------------------------------------------------A-----------------------------------------------------------------------------
+#A es agrupación solo por año tomando en cuenta como dato numérico el porcentaje de la población que utiliza servicios de 
+# saneamiento gestionados de forma segura para el calculo de los estadísticos 
+#(Media/Mediana/Variación/Dispersión/Mínimo/Máximo) del porcentaje de la población que utiliza servicios de saneamiento gestionados 
+# de forma segura
+      
+def TE_A(*op1): 
+    lis_op=[] 
+    for op in op1:
+        df_by_A = df_complete.groupby(["Anio"])[["Porcentaje"]].aggregate(op)
+        df_by_A.reset_index(inplace=True)
+        lis_op.append(df_by_A['Porcentaje'])         
+    #DataFrame con todos los estadisticos
+    com = pd.DataFrame({"Promedio":lis_op[0],"Mediana":lis_op[1],"Varianza":lis_op[2],"Desviacion_Estandar":lis_op[3],
+        "Minimo":lis_op[4],"Maximo":lis_op[5]})
+    com.set_index(df_by_A['Anio'], inplace=True)
+    print_tabulate(com.head(8))
+    
+    #Gráficas
+    for lar in com.columns: 
+        graf = plt.figure()
+        plt.plot(com.index,com[lar])
+        gxanio_format(com)
+        plt.title(lar)
+        graf.savefig(f"C:/Users/linda/OneDrive/Documentos/LINDA GAMEZ/7MO SEMESTRE/MINERÍA DE DATOS/mineria.de.datos/img/A_{lar}.png",bbox_inches='tight')
+        plt.close()
+    
+TE_A(statistics.mean,statistics.median,statistics.variance,statistics.stdev,min,max)
+
+#-------------------------------------------------------------ABC-----------------------------------------------------------------------------
+#ABC se refiere a que se pueden agregar 3 diferentes categorías 
+
+#TE_abc es una función donde se agrupan 3 diferentes categorías y se hace el calculo del promedio del porcentaje de la población que utiliza 
+# servicios de saneamiento gestionados de forma segura. La función también incluye las graficas necesarias para las agrupaciones. 
+def TE_abc(a:str,b:str,c:str): 
+    
+    df_by_abc = df_complete.groupby([a,b,c])[["Porcentaje"]].aggregate(statistics.mean)
+    df_by_abc.reset_index(inplace=True)
+    df_by_abc.set_index("Anio",inplace=True)
+    df_by_abc.rename(columns={"Porcentaje":"Promedio"},inplace=True)
+    print_tabulate(df_by_abc.head(10))
+
+    aA = a[0]+b[0]+c[0]
+
+    #Una categoría de la agrupación por boxplot
+    for cat in set(df_by_abc[b]):
+        bp1 = df_by_abc[df_by_abc[b] == cat]
+        bp1.boxplot("Promedio",by=c)
+        plt.xticks(rotation ='vertical')
+        plt.title(b+": "+cat)
+        plt.ylabel("Promedio")
+        plt.savefig(f"C:/Users/linda/OneDrive/Documentos/LINDA GAMEZ/7MO SEMESTRE/MINERÍA DE DATOS/mineria.de.datos/img/{aA}_{cat}_boxplot.png",bbox_inches='tight')
+        plt.close()
+    
+    #Graficas 
+    for cat in set(df_by_abc[b]):
+        a1 = df_by_abc[df_by_abc[b] == cat]
+        for c2 in a1[c] :
+            a2 = a1[a1[c] == c2]
+            a2.plot(y='Promedio')
+            plt.title(b+": "+cat,loc="left")
+            plt.title(c2,loc="right")
+            gxanio_format(a2)
+            plt.savefig(f"C:/Users/linda/OneDrive/Documentos/LINDA GAMEZ/7MO SEMESTRE/MINERÍA DE DATOS/mineria.de.datos/img/{aA}_Promedio_{cat}_{c2}.png",bbox_inches='tight')
+            plt.close()
+            a2.boxplot("Promedio",by=c)
+            plt.title(b+": "+cat)
+            plt.savefig(f"C:/Users/linda/OneDrive/Documentos/LINDA GAMEZ/7MO SEMESTRE/MINERÍA DE DATOS/mineria.de.datos/img/{aA}_Promedio_bplt_{cat}_{c2}.png",bbox_inches='tight')
+            plt.close()
+
+    if (a,b,c == 'Region de la OMS'): 
+        df_by_abc.rename(columns={"Region de la OMS":"Region"},inplace=True)
+        
+    if (a,b,c == 'Tipo del Area'): 
+        df_by_abc.rename(columns={"Tipo del Area":"Area"},inplace=True)
+
+    #ANOVA (no completo)
+    '''modl = ols(f'Promedio ~ Region:Pais', data=df_by_abc).fit()
+    anova_df = sm.stats.anova_lm(modl, typ=2)
+    if anova_df["PR(>F)"][0] < 0.05:
+        #print(f"Estadistico: {an}")
+        print("Hay diferencias")
+        print(anova_df)
+        # Prueba tukey
+        # imprimir los resultados
+    else:
+        #print(f"Estadistico: {an}")       
+        print("No hay diferencias")
+        print(anova_df)  '''
+
+TE_abc('Anio','Region de la OMS', 'Pais') 
+#Agrupación por año, región de la OMS y país. Contiene: 
+# Tabla donde se agrupa en cada año el promedio de los porcentajes que tiene cada país de cierta región sin tomar en cuenta el tipo de area ni
+#  la categoría en la que se encuentra.
+# Boxplots del promedio del porcentaje de la población... por región de la OMS con sus respectivos países. 
+# Plots individuales del promedio del % para cada país a través de los años
+# Boxplots individuales del promedio del % a través de los años para cada país 
+
+TE_abc('Anio', 'Categoria', 'Region de la OMS')
+TE_abc('Anio', 'Categoria', 'Tipo del Area')
+TE_abc('Anio', 'Region de la OMS', 'Tipo del Area')
+TE_abc('Anio', 'Categoria', 'Pais')
